38dd92181785eb80f0f703974acd9311b766172d
amabelle <amabelletr@gmail.com>
2022-09-02 02:43:17 +0300

    Deleted config for vercel

--- server.js
+++ server.js
@@ -31,17 +31,15 @@ app.use(express.json({ extended: false }));
 // app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version, Authorization, X-CSRF-Token, '
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PATCH, DELETE, OPTIONS'
   );
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
@@ -77,6 +75,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server runninig in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
-
-// Export the Express API in order for Vercel to turn Express into a serverless function
-module.exports = app;
